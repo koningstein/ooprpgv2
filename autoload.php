d0be4c668fa73760275d588fd189127d72721c1c
Koningstein <m.koningstein@tcrmbo.nl>
2025-07-14 15:31:18 +0200

    Opdracht 13 klaar

--- autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-spl_autoload_register(function ($class) {
-    $parts = explode('\\', $class);
-    $className = end($parts);
-    $file = __DIR__ . '/' . $className . '.php';
-    if (file_exists($file)) {
-        require_once $file;
-    }
-});
