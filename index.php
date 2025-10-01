ad33545737bf18e981dc8cb92b242ca74cb08ac1
Koningstein <m.koningstein@tcrmbo.nl>
2025-10-01 16:18:56 +0200

    Opdracht 25 klaar

--- index.php
+++ index.php
@@ -8,8 +8,10 @@ use Game\Equipment;
 use Game\Item;
 use Game\Wallet;
 use Game\Inventory;
+use Game\CharacterList;
 use Smarty\Smarty;
 
+session_start();
 // Initialize Smarty
 $smarty = new Smarty();
 $smarty->setTemplateDir('templates/');
@@ -17,6 +19,7 @@ $smarty->setCompileDir('templates_c/');
 
 // Check for 'page' GET parameter
 $page = $_GET['page'] ?? '';
+$characterList = $_SESSION['characterList'] ?? new CharacterList();
 
 // Handle POST: create character
 if ($page === 'createCharacter' && $_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -38,6 +41,9 @@ if ($page === 'createCharacter' && $_SERVER['REQUEST_METHOD'] === 'POST') {
     $character = new Character((int)$_POST['maxInventorySlots']);
     $character->setCharacter($_POST['name'], $_POST['role'], $stats);
 
+    $characterList->addCharacter($character);
+    $_SESSION['characterList'] = $characterList;
+
     $smarty->assign('newCharacter', $character);
     $smarty->display('characterCreated.tpl');
     exit;
@@ -47,72 +53,13 @@ if ($page === 'createCharacter') {
     $smarty->display('createCharacterForm.tpl');
     exit;
 }
+if ($page === 'characterList') {
+    $characters = $characterList->getCharacters();
+    $smarty->assign('characters', $characters);
+    $smarty->display('characterList.tpl');
+    exit;
+}
 
-// Jaina
-$jainaStats = new CharacterStats();
-$jainaStats->setStats(150, 20, 15) . "<br>";
-$jaina = new Character(12);
-$jaina->setCharacter("Jaina", "Warrior", $jainaStats) . "<br>";
-$jainaEquipment = new Equipment();
-$jainaEquipment->setEquipment(null, null) . "<br>";
-$jainaWallet = new Wallet();
-$jainaWallet->setGold(100) . "<br>";
-$jainaSword = new Item();
-$jainaSword->setItem("Steel Sword", "Weapon", 30, 10, 0, 0, "None") . "<br>";
-$jaina->getInventory()->addItem($jainaSword);
-$jaina->equipWeapon($jaina->getInventory()->getItem("Steel Sword"));
+$smarty->display('layout.tpl');
 
-// Aria
-$ariaStats = new CharacterStats();
-$ariaStats->setStats(110, 30, 5) . "<br>";
-$aria = new Character(8);
-$aria->setCharacter("Aria", "Archer", $ariaStats) . "<br>";
-$ariaEquipment = new Equipment();
-$ariaEquipment->setEquipment("Longbow", null) . "<br>";
-$ariaWallet = new Wallet();
-$ariaWallet->setGold(100) . "<br>";
-$ariaRing = new Item();
-$ariaRing->setItem("Magic Ring", "accessory", 250, 15) . "<br>";
-$aria->getInventory()->addItem($ariaRing);
-
-// Thorgar
-$thorgarStats = new CharacterStats();
-$thorgarStats->setStats(100, 30, 5) . "<br>";
-$thorgar = new Character();
-$thorgar->setCharacter("Thorgar", "Mage", $thorgarStats) . "<br>";
-$thorgarEquipment = new Equipment();
-$thorgarEquipment->setEquipment("Magic Staff", "Wizard Robe") . "<br>";
-$thorgarWallet = new Wallet();
-$thorgarWallet->setGold(180) . "<br>";
-$thorgarPotion = new Item();
-$thorgarPotion->setItem("Mana Potion", "consumable", 40, 0, 0, 20) . "<br>";
-$thorgar->getInventory()->addItem($thorgarPotion);
-
-$characters = [$jaina, $aria, $thorgar];
-
-// Start first fight
-$battle1 = new Battle();
-$result1 = $battle1->startFight($aria, $thorgar);
-
-// Reset health of both characters
-$aria->getStats()->resetHealth();
-$thorgar->getStats()->resetHealth();
-
-// Start second fight
-$battle2 = new Battle();
-$result2 = $battle2->startFight($aria, $thorgar);
-
-// Assign variables to Smarty
-$smarty->assign('characters', $characters);
-$smarty->assign('battleResult1', $result1);
-$smarty->assign('battleResult2', $result2);
-
-// Display the template
-$smarty->display('gameOverview.tpl');
-echo "<pre>";
-var_dump($battle1);
-//var_dump($hero);
-//var_dump($heroStats);
-//var_dump($heroEquipment);
-//var_dump($heroWallet);
-//var_dump($sword);
+$_SESSION['characterList'] = $characterList;
